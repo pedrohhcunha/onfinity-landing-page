2fc295dd5642c33078ed0ec0d2ef90f24b46adce
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-04-26 09:26:31 -0300

    Add text content in benefis section

--- components/Benefits/index.jsx
+++ components/Benefits/index.jsx
@@ -28,29 +28,29 @@ export default function Benefits(){
                     <div className={styles.areaIcon}>
                         <Image src={icon2} alt="Ícone" />
                     </div>
-                    <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
-                    <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
+                    <h4 className={styles.itemTitle}>+ Agilidade</h4>
+                    <p className={styles.itemDescription}> Dois centros de distribuição que garantem uma entrega segura no tempo necessário para o seu negócio performar.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
                         <Image src={icon3} alt="Ícone" />
                     </div>
-                    <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
-                    <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
+                    <h4 className={styles.itemTitle}>Preço de fábrica</h4>
+                    <p className={styles.itemDescription}>Condições que facilitam a compra através da política de preços de fábrica, importador ou distribuidor.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
                         <Image src={icon4} alt="Ícone" />
                     </div>
-                    <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
-                    <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
+                    <h4 className={styles.itemTitle}>Engenharia tributária</h4>
+                    <p className={styles.itemDescription}>Oferecemos uma consultoria gratuita que permite identificar a estratégia de compra que oferece os melhores benefícios fiscais e atende a sua necessidade. </p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
                         <Image src={icon5} alt="Ícone" />
                     </div>
-                    <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
-                    <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
+                    <h4 className={styles.itemTitle}>Produtos ESG</h4>
+                    <p className={styles.itemDescription}>Produtos feitos a partir de matérias-primas recicladas que entregam performance, contribuem para a cadeia sustentável e são adequados às metas de ESG.</p>
                 </li>
             </ul>
             <Button>Fale com um especialista</Button>
