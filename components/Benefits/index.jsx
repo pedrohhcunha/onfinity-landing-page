5a229dd40bcc40c9f095e57050686956ebfaf0cd
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-04-26 09:18:31 -0300

    Insert icons in benefits section

--- components/Benefits/index.jsx
+++ components/Benefits/index.jsx
@@ -1,6 +1,12 @@
 import styles from './style.module.scss'
 import SectionTitle from '../SectionTitle'
 import Button from '../Button'
+import icon1 from './images/icon1.svg'
+import icon2 from './images/icon2.svg'
+import icon3 from './images/icon3.svg'
+import icon4 from './images/icon4.svg'
+import icon5 from './images/icon5.svg'
+import Image from 'next/image'
 
 export default function Benefits(){
     return(
@@ -13,30 +19,35 @@ export default function Benefits(){
             <ul className={styles.list}>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
+                        <Image src={icon1} alt="Ícone" />
                     </div>
                     <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
                     <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
+                        <Image src={icon2} alt="Ícone" />
                     </div>
                     <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
                     <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
+                        <Image src={icon3} alt="Ícone" />
                     </div>
                     <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
                     <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
+                        <Image src={icon4} alt="Ícone" />
                     </div>
                     <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
                     <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
                 </li>
                 <li className={styles.item}>
                     <div className={styles.areaIcon}>
+                        <Image src={icon5} alt="Ícone" />
                     </div>
                     <h4 className={styles.itemTitle}>Amplo mix de soluções</h4>
                     <p className={styles.itemDescription}>Mix completo em suprimentos corporativos que entregam a competitividade necessária para a sua operação.</p>
