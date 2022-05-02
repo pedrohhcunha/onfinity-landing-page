9807684a01a3ef85d537d9b1d1a8d5b8d33b33c6
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-02 16:00:16 -0300

    Remove circles in mobile devices

--- components/HorizontalCircles/index.jsx
+++ components/HorizontalCircles/index.jsx
@@ -1,9 +1,10 @@
+import styles from './style.module.scss'
 import circles from './images/circles.svg'
 import Image from 'next/image'
 
 export default function HorizontalCircles({ top, left, right, bottom }) {
     return(
-        <aside style={{
+        <aside className={styles.circles} style={{
             position: 'absolute',
             top: top,
             left: left,
--- components/VerticalCircles/index.jsx
+++ components/VerticalCircles/index.jsx
@@ -1,9 +1,10 @@
+import styles from './style.module.scss'
 import circles from './images/circles.svg'
 import Image from 'next/image'
 
 export default function VerticalCircles({ top, left, right, bottom }) {
     return(
-        <aside style={{
+        <aside className={styles.circles} style={{
             position: 'absolute',
             zIndex: '1000',
             top: top,
