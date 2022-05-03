01508b7cec9fd1ce86237c982631a529a5a17a3d
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-03 10:45:33 -0300

    Adding more meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,17 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel='canonical' href='https://onfinity.com.br' />
         <title>Onfinity - Suprimentos corporativos</title>
+        <meta name="title" content="Onfinity - Suprimentos corporativos" />
+        <meta name="description" content="A sua melhor escolha em suprimentos corporativos. 
+        A  Onfinity  possui  um  ecossistema  que  conecta indústrias  a  clientes  finais  e  que  alia  big  data, inteligência artificial e atendimento comercial consultivo. Isso tudo para lhe ajudar a encontrar soluções nas categorias que mais importam: 
+        EPI ́s,  higiene  e  limpeza,  descartáveis,  materiais  
+        médicos,  materiais  para  escritório  e  informática,  
+        uniformes, embalagens e suprimentos industriais
+        Fabricamos, importamos e comercializamos
+        suprimentos corporativos que garantem custos 
+        competitivos, alta produtividade e performance 
+        no seu ambiente de trabalho.
+        Desde 1991 3 unidades fabris 2 CD's Sul e Sudeste Fabricação, importação e distribuição de suprimentos corporativos." />
       </Head>
       <Component {...pageProps} />
     </>
