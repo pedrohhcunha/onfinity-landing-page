32f31d01b062c8396e8239912e280901d43eba51
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-04 17:11:25 -0300

    Add metatags

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,32 @@ export default function App({ Component, pageProps }) {
   return (
     <ModalProvider>
       <Head>
-        <link rel='canonical' href='https://onfinity.com.br' />
+        <meta charset="UTF-8" />
+        <meta http-equiv="content-language" content="pt-br" />
         <meta name="description" content="A Onfinity é uma solução completa em suprimentos corporativos, fornecendo a sua empresa produtos de higiene e limpeza, epis, uniformes, materias médicos, embalagens, descatáveis e materias de escritório/informática." />
         <meta name="keywords" content="Onfinity, suprimentos, corporativos, soluções, mercado corporativos, suprimento corporativo, suprimentos corporativos office, apis, uniformes, descartáveis, papel, linha de papéis, higiene e limpeza, higiente, limpeza" />
         <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
+        <meta name="author" content="Onfinity" />
+
+        <meta property="og:type" content="company" />
+        <meta property="og:title" content="Onfinity - Soluções em suprimentos corporativos" />
+        <meta property="og:description" content="A Onfinity é uma solução completa em suprimentos corporativos, fornecendo a sua empresa produtos de higiene e limpeza, epis, uniformes, materias médicos, embalagens, descatáveis e materias de escritório/informática." />
+        <meta property="og:image" content="https://onfinity.com.br/images/cover-blue.png" />
+        <meta property="og:url" content="https://onfinity.com.br" />
+        <meta property="og:site_name" content="Onfinity" />
+        <meta property="fb:admins" content="Onfinity-101644625222997" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Onfinity - Soluções em suprimentos corporativos" />
+        <meta name="twitter:description" content="A Onfinity é uma solução completa em suprimentos corporativos, fornecendo a sua empresa produtos de higiene e limpeza, epis, uniformes, materias médicos, embalagens, descatáveis e materias de escritório/informática." />
+        <meta name="twitter:image" content="https://onfinity.com.br/images/cover-blue.png" />
+        <meta name="twitter:url" content="https://onfinity.com.br" />
+
+        <link rel='canonical' href='https://onfinity.com.br' />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+     
       </Head>
       <Component {...pageProps} />
       <ContactModal />
