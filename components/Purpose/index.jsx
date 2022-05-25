2253736393544597b57a37b5c54d9a9b7429a60b
SampleUser <pedro.cunha@onfinity.com.br>
2022-05-25 10:49:16 -0300

    Update any images

--- components/Purpose/index.jsx
+++ components/Purpose/index.jsx
@@ -1,7 +1,7 @@
 import styles from './style.module.scss'
 import Image from 'next/image'
 import SectionTitle from '../SectionTitle'
-import placeholder from './images/placeholder.webp'
+import placeholder from './images/purpose.webp'
 import HorizontalCircles from '../HorizontalCircles'
 
 export default function Purpose(){
